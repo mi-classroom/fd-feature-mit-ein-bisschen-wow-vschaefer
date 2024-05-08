a10844a49f8cde39439e421320a9623c7e7f4727
Volker Schaefer <v.schaefer@klickmeister.de>
2024-05-08 13:44:29 +0200

    feat: adds intersection observer

--- assets/scripts/modules/observer.js
+++ assets/scripts/modules/observer.js
@@ -2,8 +2,28 @@
 ---------------------------------------------------------------------------- */
 
 const addObserver = () => {
+  const elements = document.querySelectorAll('[data-js-observe]');
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5
+  };
 
+  const observer = new IntersectionObserver(callback, options);
+  elements.forEach(element => observer.observe(element));
+  console.log(elements);
 }
+
+const callback = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('is-in-view');
+    } else {
+      entry.target.classList.remove('is-in-view');
+    }
+  });
+};
+
 
 
 /* Exports
